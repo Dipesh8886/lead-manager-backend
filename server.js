eb5835012a293483aefcb115fe0ecbdc98c7cc30
Dipesh8886 <dipeshrawat0408@gmail.com>
2025-12-05 23:13:31 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [process.env.FRONTEND, process.env.FRONTEND_2],
     credentials: true,
   })
 );
